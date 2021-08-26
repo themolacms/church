fda6960db29d550d601ff2c2e6a9c8a11e03ce70
Lam Nhan <lamnhan92@gmail.com>
2021-08-26 12:31:51 +0700

    update

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/blank'),
+      dir: require('path').join(__dirname, './coverage/mola-church'),
       subdir: '.',
       reporters: [
         { type: 'html' },
