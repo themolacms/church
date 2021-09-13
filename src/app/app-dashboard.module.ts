8b09fd3ca476181db39f03506b829457ed5e7666
Lam Nhan <lamnhan92@gmail.com>
2021-09-13 18:04:33 +0700

    update dashboard

--- src/app/app-dashboard.module.ts
+++ src/app/app-dashboard.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { NguixDashboardModule, DASHBOARD_CONFIG, dashboardConfig } from '@lamnhan/nguix-dashboard';
+import { NguixDashboardModule, DASHBOARD_CONFIG, dashboardConfig, LinkingSchemaMeta } from '@lamnhan/nguix-dashboard';
 
 @NgModule({
   imports: [NguixDashboardModule],
@@ -23,10 +23,39 @@ import { NguixDashboardModule, DASHBOARD_CONFIG, dashboardConfig } from '@lamnha
         ],
         plugins: [
           dashboardService => {
+            // register article and video category types
             dashboardService.categoryPart.contentTypes.push(
               { text: 'Articles', value: 'article_category', icon: `icon-articles` },
               { text: 'Videos', value: 'video_category', icon: `icon-videos` },
             );
+            // register article type
+            dashboardService.postPart.contentTypes.push(
+              { text: 'Articles', value: 'article', icon: `icon-articles` },
+            );
+            // modify post categories
+            dashboardService.postPart.formSchema.forEach((schema, i) => {
+              if (schema.name === 'categories' && schema.meta) {
+                dashboardService.postPart.formSchema[i] = {
+                  ...schema,
+                  meta: {
+                    ...schema.meta,
+                    typeFilter: {default: 'default', article: 'article_category'},
+                  },
+                };
+              }
+            });
+            // modify video categories
+            dashboardService.videoPart.formSchema.forEach((schema, i) => {
+              if (schema.name === 'categories' && schema.meta) {
+                dashboardService.videoPart.formSchema[i] = {
+                  ...schema,
+                  meta: {
+                    ...schema.meta,
+                    typeFilter: {default: 'video_category'},
+                  },
+                };
+              }
+            });
           },
         ],
       }),
