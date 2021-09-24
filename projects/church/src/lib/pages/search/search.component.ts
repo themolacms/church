ebc7863328e9ecc6d409ec01eecc722a433ac9f6
Lam Nhan <lamnhan92@gmail.com>
2021-09-24 15:32:35 +0700

    update search page

--- projects/church/src/lib/pages/search/search.component.ts
+++ projects/church/src/lib/pages/search/search.component.ts
@@ -1,9 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Store } from '@ngxs/store';
 import { combineLatest, of } from 'rxjs';
-import { tap, switchMap } from 'rxjs/operators';
+import { take, tap, filter, map, switchMap } from 'rxjs/operators';
 import { Post, Video, Tag } from '@lamnhan/schemata';
-import { TagDataService, PostDataService, VideoDataService } from '@lamnhan/ngx-schemata';
+import {
+  TagDataService,
+  PostDataService,
+  PostSearchAction,
+  PostSearchMoreAction,
+  PostStateSearchListItem,
+  VideoDataService,
+  VideoSearchAction,
+  VideoSearchMoreAction,
+  VideoStateSearchListItem,
+} from '@lamnhan/ngx-schemata';
 import { HelperService, UserService, DatabaseDataSearchResult } from '@lamnhan/ngx-useful';
 
 @Component({
@@ -21,8 +32,29 @@ export class SearchComponent implements OnInit {
   ])
   .pipe(
     tap(([queryParams]) => {
-      if (!queryParams.q) return;
-      this.selectQuery(queryParams.q);
+      // run a new search
+      if (queryParams.q) {
+        // do searching
+        this.selectQuery(queryParams.q);
+        // TODO: remove query from url
+      }
+      // check if there are result
+      else {
+        this.store
+          .select(state => state.schemata_post.searchList?.[this.postSearchId] as PostStateSearchListItem)
+          .pipe(
+            filter(data => !!data),
+            take(1),
+          )
+          .subscribe(data => this.handlePostSearch(data));
+        this.store
+          .select(state => state.schemata_video.searchList?.[this.videoSearchId] as VideoStateSearchListItem)
+          .pipe(
+            filter(data => !!data),
+            take(1),
+          )
+          .subscribe(data => this.handleVideoSearch(data));
+      }
     }),
   );
 
@@ -30,6 +62,7 @@ export class SearchComponent implements OnInit {
   topQueries: Tag[] = [];
   query = '';
 
+  private readonly postSearchId = 'post';
   isPostLoading = false;
   isPostLoadingMore = false;
   postResult?: DatabaseDataSearchResult<Post>;
@@ -37,7 +70,8 @@ export class SearchComponent implements OnInit {
   postCount = 0;
   postTotalPages = 0;
   postPage = 1;
-  
+
+  private readonly videoSearchId = 'video';
   isVideoLoading = false;
   isVideoLoadingMore = false;
   videoResult?: DatabaseDataSearchResult<Video>;
@@ -48,6 +82,7 @@ export class SearchComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
+    private store: Store,
     private helperService: HelperService,
     private userService: UserService,
     private tagDataService: TagDataService,
@@ -56,6 +91,83 @@ export class SearchComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    this.loadTopQueries();
+  }
+
+  selectQuery(query: string) {
+    this.query = query;
+    this.search();
+  }
+
+  search() {
+    if (!this.query) return;
+    // fetch posts
+    this.isPostLoading = true;
+    this.store.dispatch(new PostSearchAction(this.postSearchId, this.query, this.viewSize))
+      .pipe(
+        take(1),
+        map(state => state.schemata_post.searchList?.[this.postSearchId] as PostStateSearchListItem),
+      )
+      .subscribe(data => this.handlePostSearch(data));
+    // fetch videos
+    this.isVideoLoading = true;
+    this.store.dispatch(new VideoSearchAction(this.videoSearchId, this.query, this.viewSize))
+      .pipe(
+        take(1),
+        map(state => state.schemata_video.searchList?.[this.videoSearchId] as VideoStateSearchListItem),
+      )
+      .subscribe(data => this.handleVideoSearch(data));
+    // save query
+    this.saveQuery(this.query);
+  }
+
+  loadMorePosts() {
+    if (!this.postResult) return;
+    this.isPostLoadingMore = true;
+    this.store.dispatch(new PostSearchMoreAction(this.postSearchId, ++this.postPage))
+    .pipe(
+      take(1),
+      map(state => state.schemata_post.searchList?.[this.postSearchId] as PostStateSearchListItem),
+    )
+    .subscribe(item => {
+      this.posts = item.items;
+      this.isPostLoadingMore = false;
+    });
+  }
+
+  loadMoreVideos() {
+    if (!this.videoResult) return;
+    this.isVideoLoadingMore = true;
+    this.store.dispatch(new VideoSearchMoreAction(this.videoSearchId, ++this.videoPage))
+    .pipe(
+      take(1),
+      map(state => state.schemata_video.searchList?.[this.videoSearchId] as VideoStateSearchListItem),
+    )
+    .subscribe(item => {
+      this.videos = item.items;
+      this.isVideoLoadingMore = false;
+    });
+  }
+
+  private handlePostSearch(data: PostStateSearchListItem) {
+    this.query = data.query;
+    this.postResult = data.result;
+    this.posts = data.items;
+    this.postCount = this.postResult.count();
+    this.postTotalPages = Math.ceil(this.postCount / this.viewSize);
+    this.isPostLoading = false;
+  }
+
+  private handleVideoSearch(data: VideoStateSearchListItem) {
+    this.query = data.query;
+    this.videoResult = data.result;
+    this.videos = data.items;
+    this.videoCount = this.videoResult.count();
+    this.videoTotalPages = Math.ceil(this.videoCount / this.viewSize);
+    this.isVideoLoading = false;
+  }
+
+  private loadTopQueries() {
     this.tagDataService.list(
       ref => ref
         .where('status', '==', 'publish')
@@ -68,57 +180,6 @@ export class SearchComponent implements OnInit {
     .subscribe(tags => this.topQueries = tags);
   }
 
-  search() {
-    if (!this.query) return;
-    // fetch posts
-    this.isPostLoading = true;
-    this.postResult = this.postDataService.search(this.query, this.viewSize);
-    if (this.postResult) {
-      this.postResult.list().subscribe(posts => {
-        this.posts = posts;
-        this.isPostLoading = false;
-      });
-      this.postCount = this.postResult.count();
-      this.postTotalPages = Math.ceil(this.postCount / this.viewSize);
-    }
-    // fetch videos
-    this.isVideoLoading = true;
-    this.videoResult = this.videoDataService.search(this.query, this.viewSize);
-    if (this.videoResult) {
-      this.videoResult.list().subscribe(videos => {
-        this.videos = videos;
-        this.isVideoLoading = false;
-      });
-      this.videoCount = this.videoResult.count();
-      this.videoTotalPages = Math.ceil(this.videoCount / this.viewSize);
-    }
-    // save query
-    this.saveQuery(this.query);
-  }
-
-  loadMorePosts() {
-    if (!this.postResult)  return;
-    this.isPostLoadingMore = true;
-    this.postResult.list(++this.postPage).subscribe(posts => {
-      this.posts = this.posts.concat(posts);
-      this.isPostLoadingMore = false;
-    });
-  }
-
-  loadMoreVideos() {
-    if (!this.videoResult)  return;
-    this.isVideoLoadingMore = true;
-    this.videoResult.list(++this.videoPage).subscribe(videos => {
-      this.videos = this.videos.concat(videos);
-      this.isVideoLoadingMore = false;
-    });
-  }
-
-  selectQuery(query: string) {
-    this.query = query;
-    this.search();
-  }
-
   private saveQuery(query: string) {
     const uid = (this.userService.uid || '');
     const title = query;
@@ -129,7 +190,7 @@ export class SearchComponent implements OnInit {
       title,
       id,
       type: 'search',
-      status: 'publish',
+      status: 'draft',
       createdAt,
       updatedAt: createdAt,
       count: 1,
